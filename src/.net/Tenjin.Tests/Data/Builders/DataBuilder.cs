6fbb3d06c474cd9ee0e2a62a43a1d59dd4aae794
Gerard Gouws <gerard.gouws@capgemini.com>
2023-06-16 12:42:26 +0200

    Added improvements.
    Added Sonarcloud support and analyses.
    Added better and more re-usable Azure yaml files.

--- src/.net/Tenjin.Tests/Data/Builders/DataBuilder.cs
+++ src/.net/Tenjin.Tests/Data/Builders/DataBuilder.cs
@@ -1,6 +1,12 @@
 ﻿namespace Tenjin.Tests.Data.Builders;
 
+/// <summary>
+/// The abstract class that enables one to build data for unit/integration testing purposes.
+/// </summary>
 public abstract class DataBuilder
 {
+    /// <summary>
+    /// Saves all data.
+    /// </summary>
     public abstract void Save();
 }
